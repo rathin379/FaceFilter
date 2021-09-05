c7d546cb9483ba3374255a13a404a98510b9b9d4
rathin379 <77762779+rathin379@users.noreply.github.com>
2021-09-05 15:45:18 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,25 @@
+function preload(){
+
+}
+
+function setup(){
+    canvas = createCanvas(500, 500);
+    canvas.center();
+    video = createCapture(VIDEO);
+    video.hide();
+
+    tint_color = "";
+}
+
+function draw(){
+    image(video, 0, 0, 500, 500);
+    tint(tint_color);
+}
+
+function take_snapshot(){
+    save('test.png');
+}
+
+function filter_tint(){
+    tint_color = document.getElementById("color_name").value;
+}
